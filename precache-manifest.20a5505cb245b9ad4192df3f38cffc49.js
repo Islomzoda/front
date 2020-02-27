b61a712fdb3a42568af3349fd574c2a126c518d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 07:14:14 +0000

    Deploy Islomzoda/front to github.com/Islomzoda/front.git:gh-pages

---
+++ precache-manifest.20a5505cb245b9ad4192df3f38cffc49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e9d85aafa1efa81450eaa97882c70f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa8bf3e509d06eaafe6",
+    "url": "/static/css/main.3c33167f.chunk.css"
+  },
+  {
+    "revision": "f94a81f982b43df123e2",
+    "url": "/static/js/2.420f6d79.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.420f6d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa8bf3e509d06eaafe6",
+    "url": "/static/js/main.40ce4586.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "9193145315295cc05698d1660e427b68",
+    "url": "/static/media/loader.91931453.gif"
+  }
+]);
